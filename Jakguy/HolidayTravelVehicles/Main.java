9e2e7dc902d4544384c1a1d6449bb3249a276a3a
Jakguy Boriboon <jakghuyb@gmail.com>
2025-02-13 14:34:45 +0700

    Created Main

--- Jakguy/HolidayTravelVehicles/Main.java
+++ Jakguy/HolidayTravelVehicles/Main.java
@@ -1,9 +1,8 @@
 
-public class HolidayTravelVehicles {
+public class Main {
 
 	public static void main(String[] args) {
 		Vehicle Vehicle1 = new Vehicle("AB101", "FakeFerrari", "BigSpeed", 2077, "Honda", 4560000, Vehicle.SoldStatus.NOT_SOLD);
 		Vehicle1.displayVehicleInfo();
 	}
-
 }
