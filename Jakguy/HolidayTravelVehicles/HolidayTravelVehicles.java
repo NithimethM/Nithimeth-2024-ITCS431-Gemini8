40057b25995af59d0ccfbf7c9e2d546c7b75dafa
Jakguy Boriboon <jakghuyb@gmail.com>
2025-02-13 14:15:50 +0700

    Implemented HolidayTravelVehicles

---
+++ Jakguy/HolidayTravelVehicles/HolidayTravelVehicles.java
@@ -0,0 +1,9 @@
+
+public class HolidayTravelVehicles {
+
+	public static void main(String[] args) {
+		Vehicle Vehicle1 = new Vehicle("AB101", "FakeFerrari", "BigSpeed", 2077, "Honda", 4560000, Vehicle.SoldStatus.NOT_SOLD);
+		Vehicle1.displayVehicleInfo();
+	}
+
+}
