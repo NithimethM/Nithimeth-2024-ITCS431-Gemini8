0c9819439a6f4584744b930085857acd5d8afd40
NithimethM <nithimeth.man@gmail.com>
2025-02-27 21:30:58 +0700

    remove nithimeth

--- Nithimeth/HolidayTravelVehicles/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    
-    public static void main(String[] args) {
-        Vehicle vehicle = new Vehicle("123ABC", "MyCar", "Model S", 2023, "Tesla", 79999.99);
-        vehicle.displayInfo();
-    }
-}
